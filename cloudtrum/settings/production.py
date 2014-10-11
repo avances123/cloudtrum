b85f47c107fc711bacee841bb744626b7c461b74
Fabio Rueda <avances123@gmail.com>
2014-10-11 19:58:59 +0200

    django-storages va lentisimo

--- cloudtrum/settings/production.py
+++ cloudtrum/settings/production.py
@@ -6,6 +6,12 @@ TEMPLATE_DEBUG = DEBUG
 
 
 
+INSTALLED_APPS += (
+    'storages',
+)
+
+
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
