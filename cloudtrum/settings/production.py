5b4519c5743201bc9fed0f7f5a77192369fa46ac
Fabio Rueda <avances123@gmail.com>
2014-10-11 18:38:55 +0200

    creo el fichero para produccion

---
+++ cloudtrum/settings/production.py
@@ -0,0 +1,17 @@
+from cloudtrum.settings.base import *
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379:0',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+            #'PASSWORD': 'secretpassword',  # Optional
+        }
+    }
+}
